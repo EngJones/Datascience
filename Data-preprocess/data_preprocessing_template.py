caadb4ea4caa0bf19c9a08ff61d1607a66b86f22
EngJones <48907850+EngJones@users.noreply.github.com>
2019-04-03 23:51:19 +0530

    Create data_preprocessing_template.py

---
+++ Data-preprocess/data_preprocessing_template.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+'Importing the dataset'
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+'Splitting the data for testing'
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0)
+
+
+'Feature scaling'
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) 
